4a9207a4f2c24721eadcef8e582ae0c77ccf3e84
Vladimir N. Silyaev <vsilyaev@gmail.com>
2023-04-14 13:14:23 +0000

    fixup! Include hardcaml toolchain

--- hardcaml/main.ml
+++ hardcaml/main.ml
@@ -1,34 +1,5 @@
 open Hardcaml
 
-type counter = { counter : Signal.t option; last_cycle_trigger : Signal.t; halfway_trigger : Signal.t }
-
-let trigger_of_cycles ~clock ~clear ~cycles_per_trigger ~active =
-  let open Signal in
-  assert (cycles_per_trigger > 2);
-  let spec = Reg_spec.create ~clock ~clear () in
-  let cycle_cnt = wire (Base.Int.ceil_log2 cycles_per_trigger) in
-  let max_cycle_cnt = cycles_per_trigger - 1 in
-  let next = mux2 (~:active |: (cycle_cnt ==:. max_cycle_cnt)) (zero (width cycle_cnt)) (cycle_cnt +:. 1) in
-  cycle_cnt <== reg spec ~enable:vdd next;
-  {
-    counter = Some cycle_cnt;
-    last_cycle_trigger = active &: (cycle_cnt ==:. max_cycle_cnt);
-    halfway_trigger = active &: (cycle_cnt ==:. cycles_per_trigger / 2);
-  }
-
-let counter ~clock ~trigger ~minimum ~maximum =
-  let open Signal in
-  assert (minimum <= maximum);
-  let spec = Reg_spec.create ~clock () in
-  let width = Signal.num_bits_to_represent maximum in
-  let range = maximum - minimum + 1 in
-  if range = 1 then Signal.of_int ~width maximum
-  else
-    let ctr_next = wire (Base.Int.ceil_log2 range) in
-    let ctr = reg ~enable:trigger spec ctr_next in
-    ctr_next <== mux2 (ctr ==:. range - 1) (zero (Signal.width ctr_next)) (ctr +:. 1);
-    Signal.uresize ctr width +:. minimum
-
 let counter_with_carry ?(base = 10) ?bits ~reset ~increment ~clock () =
   let base_bits = Base.Int.ceil_log2 base in
   let bits = Option.value ~default:base_bits bits in
--- hardcaml/top.v
+++ hardcaml/top.v
@@ -6,6 +6,14 @@ module top (
     output [3:0] anode,
     output [7:0] segment
 );
+  reg [4:0] reset_count = 4'b0;
+  wire reset;
+  assign reset = !reset_count[4];
+  always @(posedge clk) begin
+    if (!reset) begin
+      reset_count <= reset_count + 1'b1;
+    end
+  end
 
   clock TOP (
       .clk(clk),
